821b1bf504fe76d8639844dc657290f7659aff1d
Neelkamal Rana <52544208+neelkamalrana@users.noreply.github.com>
2024-02-19 19:51:28 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import '../About.css'; // Assuming you will create a corresponding CSS file
+import bikeImage from '../assets/bike.png.webp';
 
 function About() {
   return (
-    <div className="about-container" style = {{backgroundColor: '#f0f0f0'}}>
-      <h1 className="about-header">ABOUT US</h1>
-      <div className="about-content">
-        <p>
-          MoveEasy started off as a platform to address inefficiencies in the last mile logistics sector and
-          transform the way goods are transported around cities, enabling businesses to move anything on-demand.
-          We've grown manifold since then, positively impacting the productivity of businesses, creating tremendous
-          value for our partner-drivers and delivering happiness to a growing list of cities.
-        </p>
-        <p>
-          But our journey has just begun, our goal is to deliver the world's best end-to-end logistics platform
-          and revolutionize the transport logistics sector.
-        </p>
-        <img src="/path-to-your-image.jpg" alt="MoveEasy Team" className="about-image" />
-        <p>
-          MoveEasy is strongly committed towards improving the quality of life of our partner drivers. Our partners
-          enjoy flexible working hours, increased earnings and an effective work life balance along with additional
-          benefits like discounted insurance and fuel costs.
-        </p>
-        <img src="/path-to-another-image.jpg" alt="MoveEasy Operations" className="about-image" />
+    <div className='base'>
+
+      <div className='about-first'>
+        <h1>
+          ABOUT US
+        </h1>
       </div>
+
+      <div className='about-second'>
+        <text>
+          MoveEasy started off as a platform to address inefficiencies in the last mile logistics sector and transform the way goods are transported around cities, enabling businesses to move anything on-demand. We've grown manifold since then, positively impacting the productivity of businesses, creating tremendous value for our partner-drivers and delivering happiness to a growing list of cities.
+          <br>
+          </br>
+          But our journey has just begun, our goal is to deliver the world's best end-to-end logistics platform and revolutionize the transport logistics sector.
+        </text>
+        <div className='about-second img'>
+          <img src={bikeImage} alt="Bike" />
+        </div>
+      </div>
+      
+      <div className='about-third'>
+        <div className='about-third img'>
+          <img src={bikeImage} alt="Bike" />
+        </div>
+        <text>          
+          MoveEasy started off as a platform to address inefficiencies in the last mile logistics sector and transform the way goods are transported around cities, enabling businesses to move anything on-demand. We've grown manifold since then, positively impacting the productivity of businesses, creating tremendous value for our partner-drivers and delivering happiness to a growing list of cities.
+          <br>
+          </br>
+          But our journey has just begun, our goal is to deliver the world's best end-to-end logistics platform and revolutionize the transport logistics sector.  
+        </text> 
+      </div>
+
     </div>
   );
 }
 
 export default About;
-;
